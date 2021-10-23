934460a070fe6b9824487c22f77447ad878b6a96
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-10-23 18:52:03 -0300

    adicionando classe para indicar link selecionado

--- my-portfolio/src/components/Header/Header.js
+++ my-portfolio/src/components/Header/Header.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { links } from './linksArray';
 import './Header.css';
 
 function Header() {
+  const [selected, select] = useState('');
+
   return(
     <header className="portfolio-header">
       <h1>Guilherme Hermenegildo Junior</h1>
       <nav>
         <ul className="pages-list">
-          <li>Contatos</li>
-          <li>Curriculo</li>
-          <li>Habilidades</li>
-          <li>Sobre mim</li>
-          <li>Projetos</li>
+          { links.map((link) => 
+            <li>
+              <Link
+                to={ link.path }
+                className={ link.cName === selected ? 'list-item selected' : 'list-item' }
+                onClick={() => select(link.cName)}
+              >
+                {link.cName}
+              </Link>
+            </li> 
+          )}
         </ul>
       </nav>
     </header>
