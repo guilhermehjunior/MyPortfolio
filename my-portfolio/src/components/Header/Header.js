693be369225486e4d9648425a632151170a95460
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-10-24 12:47:03 -0300

    adicionando botao para abrir menu e o menu dependendo da media

--- my-portfolio/src/components/Header/Header.js
+++ my-portfolio/src/components/Header/Header.js
@@ -5,25 +5,46 @@ import './Header.css';
 
 function Header() {
   const [selected, select] = useState('');
+  const [menuOpen, setMenuOpen] = useState(false);
 
+  const linksList = (classname) => (
+    <nav>
+      <ul className={ classname }>
+        { links.map((link, index) => 
+          <li key={ index }>
+            <Link
+              to={ link.path }
+              className={ link.cName === selected ? 'list-item selected' : 'list-item' }
+              onClick={() => select(link.cName)}
+            >
+              {link.cName}
+            </Link>
+          </li> 
+        )}
+      </ul>
+    </nav>
+  );
+
+  const menuClickHandle = () => {
+    setMenuOpen(!menuOpen);
+  }
+  
   return(
     <header className="portfolio-header">
+      <i className="bi-moon" />
       <h1>Guilherme Hermenegildo Junior</h1>
-      <nav>
-        <ul className="pages-list">
-          { links.map((link) => 
-            <li>
-              <Link
-                to={ link.path }
-                className={ link.cName === selected ? 'list-item selected' : 'list-item' }
-                onClick={() => select(link.cName)}
-              >
-                {link.cName}
-              </Link>
-            </li> 
-          )}
-        </ul>
+      <i className="bi-menu-down" onClick={ menuClickHandle } /> 
+      <nav className="menu-container">
+        { linksList('pages-list') } 
       </nav>
+      { menuOpen &&
+      (<div className="hamburguer-menu">
+        <i className="bi-x-lg" onClick={ menuClickHandle } />
+        <nav>
+          { linksList('hamburguer-page-list') }
+        </nav>
+      </div>)
+      }
     </header>
   );
 }
