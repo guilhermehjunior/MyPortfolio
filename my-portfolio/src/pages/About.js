1344650474ec265d6c27e6c43843d7af365813c5
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-10-28 13:12:39 -0300

    adicionando conteudo sobre mim

--- my-portfolio/src/pages/About.js
+++ my-portfolio/src/pages/About.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 function About() {
   return(
-    <>
-    </>
+    <main>
+      <h2>Sobre Mim</h2>
+      <p>Sou natural de Florianópolis-SC, mas morei a maior parte da minha vida em Curitiba-PR. Atualmente resido na cidade de Medianeira-PR(Próxima a Foz do Iguaçu).</p>
+      <p>Sou Estudante de Desenvolvimento de Software na Trybe, empolgado por conhecimentos e desafios novos. Também sou formado em engenharia elétrica pela UFPR, que foi onde comecei a gostar de desenvolvimento.</p>
+      <p>Meu maior objetivo é realizar algo que melhore a vida das pessoas e acredito que nesse novo rumo que estou tomando surgirão diversas oportunidades.</p>
+      <p>Como <em>hobbies</em>, eu gosto de jogar(Vídeo Games, jogos de tabuleiro), ler, praticar atividades físicas, assistir filmes e cozinhar. Também gosto de assistir vídeos no youtube sobre: jogos, linguagens de programação e culinária.</p>
+      <p>Um ponto que tenho trabalhado para melhorar é: a procrastinação, tenho evitado deixar tudo para a última hora, para assim ter mais tempo para desenvolver o necessário e com uma qualidade maior.</p>
+      <img src="https://media-exp1.licdn.com/dms/image/D4D35AQHa-nCIaktp6w/profile-framedphoto-shrink_200_200/0/1631735689013?e=1635523200&v=beta&t=3Q-Z7BWKUPazM4oKXiM7wxXvXVQKhG_ehgztocQhpO8" alt="foto Guilherme H Jr"/>
+    </main>
   );
 }
 
