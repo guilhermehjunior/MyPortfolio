f8fd8410ca8f3fac649140a8c6af4706d730a1b5
Guilherme Hermenegildo Junior <guihjunior7@gmail.com>
2021-10-28 12:45:25 -0300

    Hamburguer-menu (#2)
    * adicionando botao para abrir menu e o menu dependendo da media
    
    * colocando transicao ao menu hamburguer, mudando cor dos icones
    
    * ajeitando erro no codigo para fechar o menu hamburguer

--- my-portfolio/src/components/Header/Header.js
+++ my-portfolio/src/components/Header/Header.js
@@ -6,24 +6,48 @@ import './Header.css';
 function Header() {
   const [selected, select] = useState('');
 
+  const menuClickHandle = () => {
+    const menu = document.querySelector('.hamburguer-menu');
+    menu.classList.toggle('open-menu');
+  }
+
+  const linkCLickHandle = (linkName) => {
+    const menu = document.querySelector('.hamburguer-menu');
+    menu.classList.remove('open-menu');
+    select(linkName);
+  }
+
+
+  const linksList = (classname) => (
+    <ul className={ classname }>
+      { links.map((link, index) => 
+        <li key={ index }>
+          <Link
+            to={ link.path }
+            className={ link.cName === selected ? 'list-item selected' : 'list-item' }
+            onClick={ () => linkCLickHandle(link.cName) }
+          >
+            {link.cName}
+          </Link>
+        </li> 
+      )}
+    </ul>
+  );
+  
   return(
     <header className="portfolio-header">
-      <h1>Guilherme Hermenegildo Junior</h1>
-      <nav>
-        <ul className="pages-list">
-          { links.map((link) => 
-            <li>
-              <Link
-                to={ link.path }
-                className={ link.cName === selected ? 'list-item selected' : 'list-item' }
-                onClick={() => select(link.cName)}
-              >
-                {link.cName}
-              </Link>
-            </li> 
-          )}
-        </ul>
+      <i className="bi-moon" />
+      <h1 className="my-name">Guilherme Hermenegildo Junior</h1>
+      <i className="bi-list" onClick={ menuClickHandle } /> 
+      <nav className="menu-container">
+        { linksList('pages-list') } 
       </nav>
+      <div className="hamburguer-menu">
+        <i className="bi-x-lg" onClick={ menuClickHandle } />
+        <nav>
+          { linksList('hamburguer-page-list') }
+        </nav>
+      </div>
     </header>
   );
 }
