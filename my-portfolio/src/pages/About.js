18485f8f846c9708f195ec6ff5569b484cfe733f
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-10-28 13:58:56 -0300

    fixing ortography mistakes and lint

--- my-portfolio/src/pages/About.js
+++ my-portfolio/src/pages/About.js
@@ -3,8 +3,6 @@ import React, { useState } from 'react';
 function About() {
   const [english, setEnglish]= useState(false);
 
-
-
   return(
     <main>
       {
@@ -12,11 +10,11 @@ function About() {
           <>
             <button onClick={() => setEnglish(!english)} type="button">Português</button>
             <h2>About Me</h2>
-            <p>I am from Florianópolis-SC-Brazil, but I lived most of my live in Curitiba-PR-Brazil. Nowadays I live in Medianeira-PR-Brazil(next to Foz do Iguaçu-PR-Brazil).</p>
+            <p>I am from Florianópolis-SC-Brazil, but I lived most of my life in Curitiba-PR-Brazil. Nowadays I live in Medianeira-PR-Brazil(next to Foz do Iguaçu-PR-Brazil).</p>
             <p>I am a Software Development student at Trybe, I am excited about new challenges and knowledge. I am graduated in Electric Engeneering by UFPR, where I started having interest in development.</p>
             <p>My greatest goal is to do something that improves people's lives and I belive that in this new direction I am taking, several opportunities will arise.</p>
             <p>My <em>hobbies</em> are: playing Vídeo Games and board games, reading, exercising, watching movies and cooking. I also enjoy watching videos on youtube about: games, programming languages and culinary.</p>
-            <p>Something I want to get better at is: to not procrastinate, I've been avoiding leaving every for the last days, so I can have more time do more things with more quality.</p>
+            <p>Something I am trying to avoid: procrastination, I've been avoiding leaving everything for the las few days, so I can have more time do more things and with more quality.</p>
             <img src="https://media-exp1.licdn.com/dms/image/D4D35AQHa-nCIaktp6w/profile-framedphoto-shrink_200_200/0/1631735689013?e=1635523200&v=beta&t=3Q-Z7BWKUPazM4oKXiM7wxXvXVQKhG_ehgztocQhpO8" alt="foto Guilherme H Jr"/>
           </>
         :
