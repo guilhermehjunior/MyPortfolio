f455e5235fcdf0e96876ee18ea723e2b06a94cba
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-10-28 13:32:30 -0300

    adicionando lista de habilidades

--- my-portfolio/src/pages/Habilidades.js
+++ my-portfolio/src/pages/Habilidades.js
@@ -2,8 +2,42 @@ import React from 'react';
 
 function Habilidades() {
   return(
-    <>
-    </>
+    <main>
+      <h2>Habilidades</h2>
+      <div className="abilities-container">
+        <div>
+          <h4>Desenvolvimento</h4>
+          <ul className="abilities-list">
+            <li>JavaScript(ES6)</li>
+            <li>CSS3</li>
+            <li>HTML5</li>
+            <li>React.js</li>
+            <li>MySQL</li>
+            <li>MongoDB</li>
+            <li>Node.js</li>
+            <li>GitHub</li>
+            <li>Metodologias Ágeis (Scrum, Kanban)</li>
+            <li>VBA</li>
+            <li>Ubuntu/Windows</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Gerais</h4>
+          <ul className="abilities-list">
+            <li>Aprendizado Rápido(Aprender a aprender)</li>
+            <li>Paciência</li>
+            <li>Pacote Office</li>
+            <li>Manutenção de Computadores</li>
+            <li>Boa comunicação e colaboração</li>
+            <li>AutoCad 2D</li>
+            <li>Familiaridade com Slack/Discord</li>
+            <li>Inglês Fluente</li>
+            <li>Espanhol Básico</li>
+            <li>Flexibilidade com horários e lugares</li>
+          </ul>
+        </div>
+      </div>
+    </main>
   );
 }
 
