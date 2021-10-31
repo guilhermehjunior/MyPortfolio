a53c988bde6907d6f6519c302d88d770f27e0e19
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-10-31 14:45:56 -0300

    add componente do cv em pt

---
+++ my-portfolio/src/components/CVpt/CVpt.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+function CVpt() {
+  return (
+    <>
+    <h2>Currículo - Guilherme Hermenegildo Junior</h2>
+    <article>
+      <h3>Objetivo Profissional</h3>
+      <ul>
+        <li>Ingressar em uma empresa como desenvolvedor web, para evoluir minha carreira sob a supervisão e estímulo de pessoas mais experientes.</li>
+        <li>Ter a oportunidade de crescer não somente profissionalmente, mas também pessoalmente.</li>
+        <li>Aplicar meus conhecimentos para desenvolver projetos que melhorem a vida das pessoas.</li>
+      </ul>
+    </article>
+    <article>
+      <h3>Background Acadêmico</h3>
+      <ul>
+        <li>Graduação em Engenharia Elétrica - Universidade Federal do Paraná | Curitiba / PR - Dezembro, 2017.</li>
+        <li>Intercâmbio em Energias Renováveis - University of Glasgow | Glasgow / Escócia  - Janeiro, 2014 a Janeiro, 2015.</li>
+        <li>Curso de Programação de Computadores, Desenvolvimento Web Full Stack, Desenvolvimento de Software - Trybe - Atual.</li>
+      </ul>
+    </article>
+    <article>
+      <h3>Background Profissional</h3>
+      <h4>Abril, 2015 – Fevereiro, 2016 - Fasttel Engenharia LTDA.
+      Estágio de Engenharia Elétrica.</h4>
+      <h5>Atividades:</h5>
+      <p>Projetos de subestações nas áreas de supervisão, comando, proteção, arranjos eletromecânicos, dimensionamento de cabos, barramentos, estudos de curto circuito, detalhamento de painéis elétricos, detalhes de montagem de equipamentos de manobra e transformação, diagramas trifilares/unifilares. Desenvolvimento de software para controle de documentos em Visual Basic.</p>
+      <h4>Novembro, 2012 – Dezembro, 2013 - Furukawa Industrial S.A.
+Estágio de Engenharia Elétrica.</h4>
+      <h5>Atividades:</h5>
+      <p>Suporte técnico para vendas na América Latina, Cuba, Espanha e Portugal. Análise de especificações técnicas de cabeamento estruturado; Montagem de amostras de soluções para Data Centers. Análise de editais. Desenvolvimento de softwares para auxiliar integradores em Visual Basic. Controle sobre atualizações das normativas referentes à cabeamento metálico.</p>
+      <h4>Outubro, 2010 - Julho, 2013 - Grupo PET (Programa de Educação Tutorial).</h4>
+      <h5>Atividades:</h5>
+      <p>Desenvolvimento de projetos técnicos relacionados à engenharia elétrica e de projetos voltados a ajudar a graduação. Realização de seminários mensais (prática de oratória). Treinamento em liderança de projetos.</p>
+    </article>
+    <article>
+      <h3>Habilidades</h3>
+      <ul>
+        <li>JavaScript(ES6) (intermediário)</li>
+        <li>Css3 (intermediário)</li>
+        <li>Html5 (intermediário)</li>
+        <li>VBA (intermediário)</li>
+        <li>MYSQL (básico)</li>
+        <li>MongoDB (básico)</li>
+        <li>Node.js (básico)</li>
+        <li>Microsoft Office (intermediário)</li>
+        <li>AutoCAD 2D (básico)</li>
+        <li>Manutenção de Computadores</li>
+        <li>Familiaridade com GitHub, Slack, Discord</li>
+        <li>Metodologias Ágeis (Scrum, Kanban)</li>
+      </ul>
+    </article>
+    <article>
+      <h3>Línguas</h3>
+      <ul>
+        <li>Inglês: Fala, Escrita, Leitura e Compreensão: Fluente.</li>
+        <li>Espanhol: Fala, Leitura, Escrita e Compreensão: Básico.</li>
+      </ul>
+    </article>
+    <article>
+      <p>Outras Informações: Carteira de Habilitação Categoria B (Desde 2011).</p>
+    </article>
+    </>
+  );
+}
+
+export default CVpt;
